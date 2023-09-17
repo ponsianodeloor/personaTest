d43391ae0d7a1563ce36558290c3258318db65a9
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-09-17 18:15:38 -0500

    implementacion del form builder y on Init en la clase principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup  } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'personaTest';
+
+  constructor(public fb: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+  }
 }
